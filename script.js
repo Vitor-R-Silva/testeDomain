50ec906b6cdd0259a5d32d5ad64fbdf231b8b941
Vitor-R-Silva <vitor.rsilva@sptech.school>
2024-03-23 22:23:36 -0300

    O for deu errado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function plot(){
     var x = document.getElementsByClassName("teste");
 
     for (var i = 0; i <= 100; i++){
-        x.createElement("<h1> teste </h1>")
+        document.innerHTML("teste")
     }
 }
 
